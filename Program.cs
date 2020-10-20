20fffc65638ceacb67591459e81505278460166c
Trusov Andrey <ast@htc-cs.com>
2020-10-20 12:44:26 +0400

    [add] face detection

--- Program.cs
+++ Program.cs
@@ -7,27 +7,55 @@ namespace SpaceWindow
     {
         static void Main(string[] args)
         {
-            using var capture = new VideoCapture(0);
-
-            int sleepTime = (int)Math.Round(1000 / capture.Fps);
-
-            using (var window = new Window("capture"))
+            try
             {
-                // Frame image buffer
-                var image = new Mat();
+                using var capture = new VideoCapture(0);
+                using var src = new Mat(capture.FrameWidth, capture.FrameHeight, MatType.CV_8UC4);
+                using var dst = new Mat(capture.FrameWidth, capture.FrameHeight, MatType.CV_8UC4);
+                using var gray = new Mat();
+                using var classifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
 
-                // When the movie playback reaches end, Mat.data becomes NULL.
-                while (true)
+                int sleepTime = (int)Math.Round(1000 / capture.Fps);
+                Rect[] faces;
+
+                using (var window = new Window("capture"))
                 {
-                    capture.Read(image); // same as cvQueryFrame
-                    if(image.Empty())
-                        break;
+                    while (true)
+                    {
+                        capture.Read(src);
 
-                    window.ShowImage(image);
-                    
-                    if (Cv2.WaitKey(sleepTime) != -1)
-                        break;
-                } 
+                        //поиск лиц
+                        Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+                        faces = classifier.DetectMultiScale(gray, 1.08, 2, HaarDetectionType.ScaleImage, new Size(30, 30));
+
+                        //вывод результатов
+                        src.CopyTo(dst);
+                        foreach (Rect face in faces)
+                        {
+                            var center = new Point
+                            {
+                                X = (int)(face.X + face.Width * 0.5),
+                                Y = (int)(face.Y + face.Height * 0.5)
+                            };
+                            var axes = new Size
+                            {
+                                Width = (int)(face.Width * 0.5),
+                                Height = (int)(face.Height * 0.5)
+                            };
+                            Cv2.Ellipse(dst, center, axes, 0, 0, 360, new Scalar(255, 0, 255), 4);
+                        }
+                        window.ShowImage(dst);
+
+                        //выход
+                        if (Cv2.WaitKey(sleepTime) != -1)
+                            break;
+                    } 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+                Console.ReadLine();
             }
         }
     }
