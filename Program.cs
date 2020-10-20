7b911448afc3f97a5fde22dbc287f83abf8cfb57
Trusov Andrey <ast@htc-cs.com>
2020-10-20 10:46:23 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace SpaceWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+Установка:
+
+* dotnet new console
+* dotnet add package OpenCvSharp4.Windows
