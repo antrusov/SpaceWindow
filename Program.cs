c9d2626da583dcef6d977bf2b6f217c2e5ccc354
andrey <andrey.trusov.udsu@yandex.ru>
2020-10-20 21:18:43 +0400

    [add] smoth face center

--- Program.cs
+++ Program.cs
@@ -14,12 +14,20 @@ namespace SpaceWindow
                 using var dst = new Mat(capture.FrameWidth, capture.FrameHeight, MatType.CV_8UC4);
                 using var gray = new Mat();
                 using var classifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+                //using var classifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
 
                 int sleepTime = (int)Math.Round(1000 / capture.Fps);
                 Rect[] faces;
 
                 using (var window = new Window("capture"))
                 {
+                    const double a = 0.2;
+                    var smothX = new Smoth(a);
+                    var smothY = new Smoth(a);
+                    var smothWd = new Smoth(a);
+                    var smothHg = new Smoth(a);
+                    bool init = false;
+
                     while (true)
                     {
                         capture.Read(src);
@@ -28,22 +36,62 @@ namespace SpaceWindow
                         Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
                         faces = classifier.DetectMultiScale(gray, 1.08, 2, HaarDetectionType.ScaleImage, new Size(30, 30));
 
+                        //инициализация
+                        if (!init && faces.Length > 0)
+                        {
+                            init = true;
+                            smothX.Reset(faces[0].X);
+                            smothY.Reset(faces[0].Y);
+                            smothWd.Reset(faces[0].Width);
+                            smothHg.Reset(faces[0].Height);
+                        }
+
                         //вывод результатов
                         src.CopyTo(dst);
                         foreach (Rect face in faces)
                         {
+                            {
+                                smothX.AddNewValue(face.X);
+                                smothY.AddNewValue(face.Y);
+                                smothWd.AddNewValue(face.Width);
+                                smothHg.AddNewValue(face.Height);
+                            }
+
+                            {
+                                var center = new Point
+                                {
+                                    X = (int)(face.X + face.Width * 0.5),
+                                    Y = (int)(face.Y + face.Height * 0.5)
+                                };
+                                var axes = new Size
+                                {
+                                    Width = (int)(face.Width * 0.5),
+                                    Height = (int)(face.Height * 0.5)
+                                };
+                                Cv2.Ellipse(dst, center, axes, 0, 0, 360, new Scalar(255, 0, 0), 1);
+                            }
+                        }
+
+                        {
+                            var x = smothX.Value;
+                            var y = smothY.Value;
+                            var wd = smothWd.Value;
+                            var hg = smothHg.Value;
+
                             var center = new Point
                             {
-                                X = (int)(face.X + face.Width * 0.5),
-                                Y = (int)(face.Y + face.Height * 0.5)
+                                X = (int)(x + wd * 0.5),
+                                Y = (int)(y + hg * 0.5)
                             };
                             var axes = new Size
                             {
-                                Width = (int)(face.Width * 0.5),
-                                Height = (int)(face.Height * 0.5)
+                                Width = (int)(wd * 0.5),
+                                Height = (int)(hg * 0.5)
                             };
-                            Cv2.Ellipse(dst, center, axes, 0, 0, 360, new Scalar(255, 0, 255), 4);
+                            //Cv2.Ellipse(dst, center, axes, 0, 0, 360, new Scalar(255, 0, 255), 4);
+                            Cv2.Ellipse(dst, center, new Size { Width = 5, Height = 5 }, 0, 0, 360, new Scalar(0, 0, 255), 3);
                         }
+
                         window.ShowImage(dst);
 
                         //выход
--- Smoth.cs
+++ Smoth.cs
@@ -4,27 +4,27 @@ namespace SpaceWindow {
     public class Smoth    
     {
         readonly double _a;
-        double _oldPrognose;
-        double _oldValue;
+        double _smothValue;
+        double _rawValue;
 
-        public double Value { get { return _oldPrognose; } }
+        public double Value { get { return _smothValue; } }
 
         public Smoth(double a = 0.1) {
             _a = a;
         }
 
         public void Reset(double startValue) {
-            _oldValue = startValue;
-            _oldPrognose = startValue;
+            _rawValue = startValue;
+            _smothValue = startValue;
         }
 
-        public double AddNewValue(double newValue)
+        public double AddNewValue(double newRawValue)
         {
             //https://towardsdatascience.com/simple-exponential-smoothing-749fc5631bed
-            var value = _a * _oldPrognose + (1 + _a) * _oldValue;
+            var value = _a * _smothValue + (1 - _a) * _rawValue;
 
-            _oldPrognose = value;
-            _oldValue = newValue;
+            _smothValue = value;
+            _rawValue = newRawValue;
 
             return value;
         }
