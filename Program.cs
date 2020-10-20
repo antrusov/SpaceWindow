ea4becbae4125e1d90ef0b682b69744bb8ae917c
Trusov Andrey <ast@htc-cs.com>
2020-10-20 10:53:21 +0400

    [add] video capture

--- Program.cs
+++ Program.cs
@@ -7,7 +7,28 @@ namespace SpaceWindow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var capture = new VideoCapture(0);
+
+            int sleepTime = (int)Math.Round(1000 / capture.Fps);
+
+            using (var window = new Window("capture"))
+            {
+                // Frame image buffer
+                var image = new Mat();
+
+                // When the movie playback reaches end, Mat.data becomes NULL.
+                while (true)
+                {
+                    capture.Read(image); // same as cvQueryFrame
+                    if(image.Empty())
+                        break;
+
+                    window.ShowImage(image);
+                    
+                    if (Cv2.WaitKey(sleepTime) != -1)
+                        break;
+                } 
+            }
         }
     }
 }
