20ac01171fea12b167fc5d0ab8563b8379ee8a3d
andrey <andrey.trusov.udsu@yandex.ru>
2021-01-08 18:27:57 +0400

    [add] рисование треугольника при помощи шейдеров

--- TrinangleWindow.cs
+++ TrinangleWindow.cs
@@ -7,6 +7,26 @@ namespace SpaceWindow
 {
     public class TrinangleWindow : GameWindow
     {
+        #region [ данные ]
+
+        //вершины треугольника
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f, //Bottom-right vertex
+            0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        //id вершинного обьекта (VBO) с данными треугольника загруженного на видеокарту
+        int VertexBufferObject;
+
+        //id обьекта, хранящего настройки вершин
+        int VertexArrayObject;
+
+        //шейдерная программа
+        Shader shader;
+
+        #endregion
+
         public TrinangleWindow(int width, int height, string title)
             :base(width, height, new GraphicsMode(), title)
         {
@@ -14,14 +34,52 @@ namespace SpaceWindow
 
         protected override void OnLoad(EventArgs e)
         {
-            GL.ClearColor(0.1f, 0.2f, 0.3f, 1f);
+            //отладочная информация
+            Console.WriteLine(GL.GetString(StringName.Extensions));
             Console.WriteLine(GL.GetString(StringName.Version));
+
+            //общие настройки
+            this.VSync = VSyncMode.Off;
+            GL.ClearColor(0.1f, 0.2f, 0.3f, 1f);
+
+            //настройка данных
+            VertexBufferObject = GL.GenBuffer(); //создать id для VBO
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject); //считать этот VBO массивом
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw); //закачать на карту данные о вершинах
+
+            VertexArrayObject = GL.GenVertexArray(); //создать VAO для хранения настроек вершин
+            GL.BindVertexArray(VertexArrayObject); //активировать вновь созданный VAO
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0); //описание того, как трактовать данные в VBO
+            GL.EnableVertexAttribArray(0); //включить 0 переменную в вершинном шейдере
+
+            //настройка шейдера
+            shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");            
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
             SwapBuffers();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            shader.Dispose();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+            base.OnUnload(e);
         }
     }
 }
